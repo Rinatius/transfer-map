4bee9b5585207342ca03c8b057a6431ce822871c
Rinatius <rinat@kloop.kg>
2020-06-12 17:41:01 +0600

    Updates

---
+++ precache-manifest.d81d89796b3d2050ab062eb33727c7ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfac5838ace77801ae8a45a0a1849fe8",
+    "url": "/transfer-map/index.html"
+  },
+  {
+    "revision": "e660942ad68159aea3c9",
+    "url": "/transfer-map/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e0f27cbbfb61e57c275f",
+    "url": "/transfer-map/static/js/2.13c5c7bf.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/transfer-map/static/js/2.13c5c7bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e660942ad68159aea3c9",
+    "url": "/transfer-map/static/js/main.ac534cf4.chunk.js"
+  },
+  {
+    "revision": "e420262724a31f209c4c",
+    "url": "/transfer-map/static/js/runtime-main.897c8ca9.js"
+  }
+]);
