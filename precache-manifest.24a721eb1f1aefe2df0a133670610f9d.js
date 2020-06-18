c199ef09d4bfe528afcdd915626f1a2b9a4db74b
aTNa <atay.narynov@gmail.com>
2020-06-18 13:36:47 +0600

    Updates

---
+++ precache-manifest.24a721eb1f1aefe2df0a133670610f9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583005515f325c80c4a1061cd9c17eff",
+    "url": "/transfer-map/index.html"
+  },
+  {
+    "revision": "10d845e16002a20754b5",
+    "url": "/transfer-map/static/css/2.294a05d7.chunk.css"
+  },
+  {
+    "revision": "953f9ffcc20974606c6b",
+    "url": "/transfer-map/static/css/main.456c1173.chunk.css"
+  },
+  {
+    "revision": "10d845e16002a20754b5",
+    "url": "/transfer-map/static/js/2.b36f06ce.chunk.js"
+  },
+  {
+    "revision": "a3b889260cce3a3f17b6182b829b261f",
+    "url": "/transfer-map/static/js/2.b36f06ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "953f9ffcc20974606c6b",
+    "url": "/transfer-map/static/js/main.b172bef2.chunk.js"
+  },
+  {
+    "revision": "12d23e0ae028e733a23e",
+    "url": "/transfer-map/static/js/runtime-main.a3ccc828.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/transfer-map/precache-manifest.37426b179de4ba740576e14296d31c37.js"
+  "/transfer-map/precache-manifest.24a721eb1f1aefe2df0a133670610f9d.js"
 );
 
 self.addEventListener('message', (event) => {
