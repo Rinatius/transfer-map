bedc26e4fa0c2d04f26da6999698b367a0bab4a6
Mark Nightingale <mark.nightingale@occrp.org>
2020-06-23 17:09:03 +0200

    Fix dropdown menus

--- src/config.js
+++ src/config.js
@@ -26,7 +26,7 @@ const formatter = new Intl.NumberFormat('en-US', {
 
 const tableOptions = {
 
-  csvUrl: 'https://cdn.test.occrp.org:10443/projects/plunder-transfer-map/data/live-data.csv',
+  csvUrl: 'https://firebasestorage.googleapis.com/v0/b/newagent-b0720.appspot.com/o/transfer-map%2Fmockup_v3.csv?alt=media&token=3294afb4-be79-487a-938e-e919b7969230',
 
   mapOptions: {
     "active_color": "#dbabac",
